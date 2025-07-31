6e2905a546ef97264f97ee1ac405ad8637166dce
learning-botman <evansamwelnyamu@gmail.com>
2025-07-31 21:22:35 +0300

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-```javascript
-document.getElementById("contact-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Thank you for contacting us! We will get back to you soon.");
-});
-```
